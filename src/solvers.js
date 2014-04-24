3871c443b66be68f1114e4c970cc68f78f54f9a9
Hack Reactor Students <theamarpatel@gmail.com>
2014-04-23 17:07:17 -0700

    Finished  countNRooksSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -24,14 +24,15 @@ window.addOnePiece = function(board) {
   var boardSlice = function (board) {
     var temp = [];
     for (var i=0; i<n; i++) {
-      temp.push(board.attributes[i]);
+      temp.push(board.attributes[i].slice());
     }
-    return new Board(temp);
+    var newBoard = new Board(temp);
+    return newBoard;
   };
 
   //i === rows
-  //j === columns
   for (i=0;i<n;i++) {
+    //j === columns
     for (j=0;j<n;j++) {
       if (matrix[i][j] === 1) {
         continue;
@@ -52,10 +53,56 @@ window.addOnePiece = function(board) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var tree = new Tree();
+  var boards = addOnePiece(new Board({n:n}));
+  for(var i = 0; i < boards.length; i++){
+    var treeNode = new TreeNode(boards[i]);
+    treeNode.parent = tree;
+    treeNode.pieces++;
+    tree.children.push(treeNode);
+  }
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+
+
+  var boardsToNodes = function (boards, node) {
+    var results = [];
+    for (var i=0;i<boards.length;i++) {
+      var newNode = new TreeNode(boards[i]);
+      newNode.parent = node;
+      newNode.pieces = node.pieces + 1;
+      results.push(newNode);
+    }
+    return results;
+  };
+
+
+
+
+
+  //contains all valid boards with n pieces
+  var results = [];
+  var recurse = function (node) {
+    for (var i=0;i<node.children.length;i++) {
+      if (node.children[i].pieces === n) {
+        results.push(node.children[i].board);
+      } else {
+        node.children[i].children = boardsToNodes(addOnePiece(node.children[i].board), node.children[i]);
+        recurse(node.children[i]);
+      }
+    }
+  };
+  recurse(tree);
+
+  var removeDuplicates = {};
+  for(var i = 0; i < results.length; i++){
+    removeDuplicates[JSON.stringify(results[i])] = true;
+  }
+  return Object.keys(removeDuplicates).length;
+
+
+
+
+
 };
 
 
--- src/tree.js
+++ src/tree.js
@@ -1,1 +1,10 @@
-tree.js
+var Tree = function () {
+  this.children = [];
+};
+
+var TreeNode = function (board) {
+  this.board = board;
+  this.children = [];
+  this.parent = null;
+  this.pieces = 0;
+};
